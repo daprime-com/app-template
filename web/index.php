3d3fea69929b7ad2fe65d4321fa8b9fcfc225000
moicirk <imurujev@gmail.com>
2015-08-21 13:12:58 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -8,5 +8,10 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
-
 (new yii\web\Application($config))->run();
+
+// After
+Yii::setAlias('@root', $rootPath); //root alias is required for builder to work
+
+$configBuilder = new \yii\configbuilder\ConfigBuilder();
+(new yii\web\Application($configBuilder->getWebConfig()))->run();
